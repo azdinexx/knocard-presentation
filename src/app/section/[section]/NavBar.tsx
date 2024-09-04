2760725088aabbd55cd63594c6e090d9c6dd2042
azdine <azdinebouali2002@gmail.com>
2024-09-04 15:05:50 +0100

    fixed NavBar

--- src/app/section/[section]/NavBar.tsx
+++ src/app/section/[section]/NavBar.tsx
@@ -12,16 +12,15 @@ const Navbar = ({
     color: string
 }) => {
     return (
-        <nav className='fixed bottom-6 left-0 w-full  '>
+        <nav className='fixed bottom-4 sm:bottom-6 left-0 w-full px-4 sm:px-6'>
             <div className='flex justify-between items-center max-w-screen-lg mx-auto w-full'>
-
                 <Link href="/">
                     <motion.button
                         whileHover={{ scale: 1.1 }}
                         whileTap={{ scale: 0.9 }}
                         className="flex flex-col items-center"
                     >
-                        <Home fill={color} width={60} height={60} />
+                        <Home fill={color} width={40} height={40} className="w-8 h-8 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16" />
                     </motion.button>
                 </Link>
                 <motion.button
@@ -30,7 +29,7 @@ const Navbar = ({
                     className="flex flex-col items-center"
                     onClick={() => {/* Add share functionality */ }}
                 >
-                    <Share fill={color} width={60} height={60} />
+                    <Share fill={color} width={40} height={40} className="w-8 h-8 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16" />
                 </motion.button>
                 <Link href="/">
                     <motion.div
@@ -38,11 +37,10 @@ const Navbar = ({
                         whileTap={{ scale: 0.9 }}
                         className="flex flex-col items-center"
                     >
-                        <Knocard fill={color} width={60} height={60} />
+                        <Knocard fill={color} width={40} height={40} className="w-8 h-8 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16" />
                     </motion.div>
                 </Link>
             </div>
-
         </nav>
     );
 };
